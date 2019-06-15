1725956179ef221b3d28585dd0d4b33fafb0e72f
Sungbin Oh <sungbin.oh@cern.ch>
2019-06-15 22:06:59 +0900

    2D HEM DATA plots added

--- copy_input.sh
+++ copy_input.sh
@@ -1,20 +1,20 @@
 cp_2018_bkg="cp "${path_2018}'*.root '$FILE_PATH'/2018/Background/'
 cp_2018_DATA="cp "${path_2018}'/DATA/HN_pair_all_SkimTree_LRSMHighPt_data*.root '$FILE_PATH'/2018/DATA/'
 echo "copying 2018 Background"
-eval $cp_2018
+eval $cp_2018_bkg
 echo "copying 2018 DATA"
 eval $cp_2018_DATA
 
 cp_2017_bkg="cp "${path_2017}'*.root '$FILE_PATH'/2017/Background/'
 cp_2017_DATA="cp "${path_2017}'/DATA/HN_pair_all_SkimTree_LRSMHighPt_data*.root '$FILE_PATH'/2017/DATA/'
 echo "copying 2017 Background"
-eval $cp_2017
+eval $cp_2017_bkg
 echo "copying 2017 DATA"
 eval $cp_2017_DATA
 
 cp_2016_bkg="cp "${path_2016}'*.root '$FILE_PATH'/2016/Background/'
 cp_2016_DATA="cp "${path_2016}'/DATA/HN_pair_all_SkimTree_LRSMHighPt_data*.root '$FILE_PATH'/2016/DATA/'
 echo "copying 2016 Background"
-eval $cp_2016
+eval $cp_2016_bkg
 echo "copying 2016 DATA"
 eval $cp_2016_DATA
--- src/HEM_issue.C
+++ src/HEM_issue.C
@@ -37,9 +37,9 @@ void openfile_DATA(TString cyclename, TString samplename, TString dir, TString h
     current_hist -> SetDirectory(0);
   }
   TH1::AddDirectory(kFALSE);
-
-  mapfunc[histname + cyclename + samplename] = current_hist;
-
+  
+  mapfunc[histname + cyclename + samplename + "_" + TString::Itoa(tag_year,10)] = current_hist;
+  
   current_file -> Close();
   delete current_file;
 }
@@ -78,103 +78,165 @@ void openfile_DATA_2D(TString cyclename, TString samplename, TString dir, TStrin
   }
   TH1::AddDirectory(kFALSE);
   cout << "[openfile_DATA_2D] " + histname + cyclename + samplename << endl;
-  maphist2D[histname + cyclename + samplename] = current_hist;
+  maphist2D[histname + cyclename + samplename + "_" + TString::Itoa(tag_year,10)] = current_hist;
   
   current_file -> Close();
   delete current_file;
 }
 
-void draw_2D_DATA(TString nameofhistogram, TString current_data, int tag_year){
+void draw_2D_DATA(TString nameofhistogram, TString current_data){
   
   cout << "[draw_2D_DATA] nameofhistogram :" << nameofhistogram << endl;
-  double total_lumi = 35.9;
-  if(tag_year == 2017) total_lumi = 41.3;
-  if(tag_year == 2018) total_lumi = 59.7;
+  double total_lumi_2016 = 35.9;
+  double total_lumi_2017 = 41.3;
+  double total_lumi_2018 = 59.7;
 
-  TString pad1 = nameofhistogram;
-  TString pad2 = nameofhistogram;
-  TString canvas = nameofhistogram;
-  TString hstack = nameofhistogram;
-  TString legend = nameofhistogram;
-  TString func = nameofhistogram;
-  TString clone = nameofhistogram;
-  TString line = nameofhistogram;
-  pad1.Insert(0, "pad1_");
-  pad2.Insert(0, "pad2_");
-  canvas.Insert(0, "c_");
-  hstack.Insert(0, "hs_");
-  legend.Insert(0, "legend_");
-  func.Insert(0, "ratio_");
-  clone.Insert(0, "h3_");
-  line.Insert(0, "l_");
-
-  mapcanvas[canvas] = new TCanvas(canvas,"",800,800);
-  canvas_margin(mapcanvas[canvas]);
+  TCanvas *canvas = new TCanvas(nameofhistogram + "canvas","",800,800);
+  canvas_margin(canvas);
   gStyle -> SetOptStat(1111);
   
   cout << "[draw_2D_DATA] nameofhistogram + Cycle_name + current_data : " << nameofhistogram + Cycle_name + current_data << endl;
-  if(!maphist2D[nameofhistogram + Cycle_name + current_data]) return;
-  maphist2D[nameofhistogram + Cycle_name + current_data] -> Scale(1. / total_lumi);
+  if(!maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"]) return;
+  if(!maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"]) return;
+  if(!maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"]) return;
+  
+  /*
+  maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> Scale(1. / total_lumi_2016);
+  maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> Scale(1. / total_lumi_2017);
+  maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> Scale(1. / total_lumi_2018);
+  */
   //maphist2D[nameofhistogram + Cycle_name + current_data] -> SetStats(0);
   //maphist2D[nameofhistogram + Cycle_name + current_data] -> GetXaxis() -> SetTitle(nameofhistogram);
   //maphist2D[nameofhistogram + Cycle_name + current_data] -> GetYaxis() -> SetTitle(nameofhistogram);
   
   //maphist2D[nameofhistogram + Cycle_name + current_data] -> Draw("COLZTEXT");
-  int bin[4];
-  bin[0] = maphist2D[nameofhistogram + Cycle_name + current_data] -> FindBin(1.5, 1.5);
-  bin[1] = maphist2D[nameofhistogram + Cycle_name + current_data] -> FindBin(-1.5, 1.5);
-  bin[2] = maphist2D[nameofhistogram + Cycle_name + current_data] -> FindBin(-1.5, -1.5);
-  bin[3] = maphist2D[nameofhistogram + Cycle_name + current_data] -> FindBin(1.5, -1.5);
+  int bin_2016[4];
+  bin_2016[0] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> FindBin(1.5, 1.5);
+  bin_2016[1] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> FindBin(-1.5, 1.5);
+  bin_2016[2] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> FindBin(-1.5, -1.5);
+  bin_2016[3] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> FindBin(1.5, -1.5);
+  
+  int bin_2017[4];
+  bin_2017[0] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> FindBin(1.5, 1.5);
+  bin_2017[1] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> FindBin(-1.5, 1.5);
+  bin_2017[2] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> FindBin(-1.5, -1.5);
+  bin_2017[3] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> FindBin(1.5, -1.5);
+
+  int bin_2018[4];
+  bin_2018[0] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> FindBin(1.5, 1.5);
+  bin_2018[1] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> FindBin(-1.5, 1.5);
+  bin_2018[2] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> FindBin(-1.5, -1.5);
+  bin_2018[3] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> FindBin(1.5, -1.5);
+  
   
   int N = 4;
-  double bin_content[4], bin_error[4];
+  double bin_content_2016[4], bin_error_2016[4];
+  double bin_content_2017[4], bin_error_2017[4];
+  double bin_content_2018[4], bin_error_2018[4];
+
   double x[4] = {0.5, 1.5, 2.5, 3.5};
   double ex[4] = {0.5, 0.5, 0.5, 0.5};
   for(int i_2D = 0; i_2D < 4; i_2D++){
-    cout << i_2D + 1 << " quadrature : " << maphist2D[nameofhistogram + Cycle_name + current_data] -> GetBinContent(bin[i_2D]) << endl;;
-    bin_content[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data] -> GetBinContent(bin[i_2D]);
-    bin_error[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data] -> GetBinError(bin[i_2D]);
+    //cout << i_2D + 1 << " quadrature : " << maphist2D[nameofhistogram + Cycle_name + current_data] -> GetBinContent(bin[i_2D]) << endl;;
+    bin_content_2016[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> GetBinContent(bin_2016[i_2D]);
+    bin_content_2017[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> GetBinContent(bin_2017[i_2D]);
+    bin_content_2018[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> GetBinContent(bin_2018[i_2D]);
+
+    bin_error_2016[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2016"] -> GetBinError(bin_2016[i_2D]);
+    bin_error_2017[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2017"] -> GetBinError(bin_2017[i_2D]);
+    bin_error_2018[i_2D] = maphist2D[nameofhistogram + Cycle_name + current_data + "_2018"] -> GetBinError(bin_2018[i_2D]);
+    
   }
-  TGraphErrors *gr = new TGraphErrors(4,x,bin_content,ex,bin_error);
-  gr->SetTitle("");
-  gr->SetMarkerColor(4);
-  gr->SetMarkerStyle(21);
+  
+  double denom_2016 = bin_content_2016[0];
+  double denom_2017 = bin_content_2017[0];
+  double denom_2018 = bin_content_2018[0];
+
+  for(int i_2D = 0; i_2D < 4; i_2D++){
+    cout << i_2D + 1 <<" quadrature : " << bin_content_2016[i_2D] << endl;
+    bin_content_2016[i_2D] = bin_content_2016[i_2D] / denom_2016;
+    bin_content_2017[i_2D] = bin_content_2017[i_2D] / denom_2017;
+    bin_content_2018[i_2D] = bin_content_2018[i_2D] / denom_2018;
+    
+    bin_error_2016[i_2D] = bin_error_2016[i_2D] / denom_2016;
+    bin_error_2017[i_2D] = bin_error_2017[i_2D] / denom_2017;
+    bin_error_2018[i_2D] = bin_error_2018[i_2D] / denom_2018;
+    
+    cout << i_2D + 1 << " quadrature : " << bin_content_2016[i_2D] << endl;
+  }
+  
+  TGraphErrors *gr_2016 = new TGraphErrors(4,x,bin_content_2016,ex,bin_error_2016);
+  gr_2016->SetTitle("");
+  gr_2016->SetMarkerColor(4);
+  gr_2016->SetMarkerStyle(21);
   
   std::vector<TString> x_labels = {"1st", "2nd", "3rd", "4th"};
   for(int i = 0; i < 4; i++){
-    gr->GetXaxis()->SetBinLabel(gr->GetXaxis()->FindBin(i + 0.5), x_labels[i]);
+    gr_2016->GetXaxis()->SetBinLabel(gr_2016->GetXaxis()->FindBin(i + 0.5), x_labels[i]);
   }
   
-  double y_mean = 0., ye_mean = 0.;
+  double max_y = 0.;
+  double min_y = 99999999.;
   for(int i = 0; i < 4; i++){
-    y_mean += bin_content[i];
-    ye_mean += bin_error[i];
+    if(max_y < bin_content_2016[i]) max_y = bin_content_2016[i];
+    if(min_y > bin_content_2016[i]) min_y = bin_content_2016[i];
   }
-  y_mean = y_mean / 4.;
-  ye_mean = ye_mean / 4.;
-  double y_range_bottom = y_mean - 10. * ye_mean;
-  double y_range_top = y_mean + 10. * ye_mean;
-
-
-
-  gr -> GetXaxis() -> SetTitleSize(0.15);
-  gr -> GetXaxis() -> SetTitleOffset(0.5);
-  gr -> GetXaxis() -> SetTitle("");
-  gr -> GetXaxis() -> LabelsOption("h"); 
-  gr -> GetYaxis() -> SetTitle("Count per 1 fb^{-1}");
-  gr -> GetYaxis() -> SetTitleSize(0.07);
-  gr -> GetYaxis() -> SetTitleOffset(1.02);
-  gr -> GetYaxis() -> SetLabelSize(0.05);
-  gr -> GetYaxis() -> SetRangeUser(y_range_bottom, y_range_top);
-
-
-  gr->Draw("ALP");
+  for(int i = 0; i < 4;i++){
+    if(max_y < bin_content_2017[i]) max_y = bin_content_2017[i];
+    if(min_y > bin_content_2017[i]) min_y = bin_content_2017[i];
+  }for(int i = 0; i < 4;i++){
+    if(max_y < bin_content_2018[i]) max_y = bin_content_2018[i];
+    if(min_y > bin_content_2018[i]) min_y = bin_content_2018[i];
+  }
   
+  double y_range_bottom = 0.;
+  double y_range_top = max_y * 2.;
+
+  gr_2016 -> SetMarkerColor(kBlue);
+  gr_2016 -> SetLineColor(kBlue);
+  gr_2016 -> GetXaxis() -> SetTitleSize(0.15);
+  gr_2016 -> GetXaxis() -> SetTitleOffset(0.5);
+  gr_2016 -> GetXaxis() -> SetTitle("");
+  gr_2016 -> GetXaxis() -> LabelsOption("h"); 
+  gr_2016 -> GetYaxis() -> SetTitle("Count / 1st Quadrature");
+  gr_2016 -> GetYaxis() -> SetTitleSize(0.07);
+  gr_2016 -> GetYaxis() -> SetTitleOffset(1.02);
+  gr_2016 -> GetYaxis() -> SetLabelSize(0.05);
+  gr_2016 -> GetYaxis() -> SetRangeUser(y_range_bottom, y_range_top);
+
+
+  gr_2016->Draw("ALP");
+
+
+  TGraphErrors *gr_2017 = new TGraphErrors(4,x,bin_content_2017,ex,bin_error_2017);
+  gr_2017 -> SetMarkerStyle(21);
+  gr_2017 -> SetMarkerColor(kRed);
+  gr_2017 -> SetLineColor(kRed);
+  gr_2017 -> Draw("LPsame");
+
+  TGraphErrors *gr_2018 = new TGraphErrors(4,x,bin_content_2018,ex,bin_error_2018);
+  gr_2018 -> SetMarkerStyle(21);
+  gr_2018 -> SetMarkerColor(kBlack);
+  gr_2018 -> SetLineColor(kBlack);
+  gr_2018 -> Draw("LPsame");
+  
+  TLegend *legend = new TLegend(0.60, 0.75, 0.95, 0.95);
+  legend -> AddEntry(gr_2016, "2016", "lp");
+  legend -> AddEntry(gr_2017, "2017", "lp");
+  legend -> AddEntry(gr_2018, "2018", "lp");
+
+  legend -> SetFillColor(kWhite);
+  legend -> SetLineColor(kBlack);
+  legend -> SetBorderSize(1);
+  legend -> SetFillStyle(1001);
+  legend -> SetShadowColor(0); 
+  legend -> SetEntrySeparation(0.3);
+  legend -> Draw("same");
   
   ////////////////////////////////////
   // -- Latex
   ////////////////////////////////////
-  mapcanvas[canvas] -> cd();
+  canvas -> cd();
   TLatex latex_CMSPriliminary, latex_Lumi;
   latex_CMSPriliminary.SetNDC();
   latex_Lumi.SetNDC();
@@ -184,25 +246,25 @@ void draw_2D_DATA(TString nameofhistogram, TString current_data, int tag_year){
   TString total_lumi_str = "41.3";
   if(tag_year == 2016) total_lumi_str = "35.9";
   if(tag_year == 2018) total_lumi_str = "59.7";
-  latex_Lumi.DrawLatex(0.7, 0.96, total_lumi_str + " fb^{-1} (13 TeV)");
-
+  latex_Lumi.DrawLatex(0.7, 0.96, "(13 TeV)");
   
-
   TString pdfname;
   TString WORKING_DIR = getenv("PLOTTER_WORKING_DIR");
   pdfname = WORKING_DIR + "/plots/HEM_issue/";
   pdfname.Append(nameofhistogram);
   pdfname.Append("_1D.pdf");
   
-  mapcanvas[canvas] -> SaveAs(pdfname);
+  canvas -> SaveAs(pdfname);
   
 }
 
-void TwoD_Yield(int tag_year){
+void draw_1D_DATA(TString nameofhistogram, TString current_data){
+
   
-  SingleMuon = "data_SingleMuon";
-  DoubleEG = "data_DoubleEG";
-  if(tag_year == 2018) DoubleEG = "data_EGamma";
+
+}
+
+void TwoD_Yield(){
   
   int N_2D_plst = 6;
   TString list_2D_lots[] = {"FatJet_0phi_VS_eta", "FatJet_1phi_VS_eta",
@@ -238,12 +300,20 @@ void TwoD_Yield(int tag_year){
 	current_channel = channels[i_channels];
 	current_dir = current_dir + "_" + current_channel + "_central";
 	
-	openfile_DATA_2D(Cycle_name, DoubleEG, current_dir, current_2D_plot, tag_year);
-	openfile_DATA_2D(Cycle_name, SingleMuon, current_dir, current_2D_plot, tag_year);
+	SingleMuon = "data_SingleMuon";
+        DoubleEG = "data_DoubleEG";
+
+	openfile_DATA_2D(Cycle_name, DoubleEG, current_dir, current_2D_plot, 2016);
+	openfile_DATA_2D(Cycle_name, SingleMuon, current_dir, current_2D_plot, 2016);
+
+	openfile_DATA_2D(Cycle_name, DoubleEG, current_dir, current_2D_plot, 2017);
+	openfile_DATA_2D(Cycle_name, SingleMuon, current_dir, current_2D_plot, 2017);
 	
-	if(channels[i_channels].Contains("DiEle")) draw_2D_DATA(current_2D_plot, DoubleEG, tag_year);
-	else draw_2D_DATA(current_2D_plot, SingleMuon, tag_year);
+	openfile_DATA_2D(Cycle_name, DoubleEG, current_dir, current_2D_plot, 2018);
+        openfile_DATA_2D(Cycle_name, SingleMuon, current_dir, current_2D_plot, 2018);
 	
+	if(channels[i_channels].Contains("DiEle")) draw_2D_DATA(current_2D_plot, DoubleEG);
+	else draw_2D_DATA(current_2D_plot, SingleMuon);
 	
       }
     }
@@ -253,11 +323,78 @@ void TwoD_Yield(int tag_year){
   
 }
 
+
+void OneD_Kinematics(){
+
+  int N_1D_plst = 8;
+  TString list_1D_lots[] = {"FatJet_0_Pt_1st_Quadrant", 
+			    "FatJet_0_Pt_2nd_Quadrant",
+			    "FatJet_0_Pt_3rd_Quadrant",
+			    "FatJet_0_Pt_4th_Quadrant",
+			    "Jet_0_Pt_1st_Quadrant",
+			    "Jet_0_Pt_2nd_Quadrant",
+			    "Jet_0_Pt_3rd_Quadrant",
+			    "Jet_0_Pt_4th_Quadrant",
+  };
+
+  int N_regions = 3;
+  TString regions[] = {"CR_Zmass",
+		       "CR_ttbar",
+		       //"CR_inv_mll",
+                       "SR"
+  };
+
+
+  int N_channels = 3;
+  TString channels[] = {"DiEle",
+                        "DiMu",
+                        "EMu"
+  };
+  
+  for(int i_list_1D = 0; i_list_1D < N_1D_plst; i_list_1D++){
+    for(int i_regions = 0; i_regions < N_regions; i_regions++){
+      for(int i_channels = 0; i_channels < N_channels; i_channels++){
+
+	TString current_1D_plot = list_1D_lots[i_list_1D] + "_" + regions[i_regions] + "_" + channels[i_channels] + "_central";
+        cout << current_1D_plot << endl;
+
+	TString current_dir = "empty";
+	TString current_channel = "empty";
+
+	current_dir = regions[i_regions];
+	current_channel = channels[i_channels];
+	current_dir = current_dir + "_" + current_channel + "_central";
+
+	SingleMuon = "data_SingleMuon";
+	DoubleEG = "data_DoubleEG";
+	
+	openfile_DATA(Cycle_name, DoubleEG, current_dir, current_1D_plot, 2016);
+	openfile_DATA(Cycle_name, SingleMuon, current_dir, current_1D_plot, 2016);
+
+	openfile_DATA(Cycle_name, DoubleEG, current_dir, current_1D_plot, 2017);
+	openfile_DATA(Cycle_name, SingleMuon, current_dir, current_1D_plot, 2017);
+
+	openfile_DATA(Cycle_name, DoubleEG, current_dir, current_1D_plot, 2018);
+	openfile_DATA(Cycle_name, SingleMuon, current_dir, current_1D_plot, 2018);
+	
+        if(channels[i_channels].Contains("DiEle")) draw_1D_DATA(current_1D_plot, DoubleEG);
+        else draw_1D_DATA(current_1D_plot, SingleMuon);
+	
+      }
+    }
+  }
+
+  return;
+  
+
+
+}
+
 void HEM_issue(){
   
   Cycle_name = "HN_pair_all_SkimTree_LRSMHighPt";
 
-  TwoD_Yield(2018);
-  
+  TwoD_Yield();
+  OneD_Kinematics();
 
 }
