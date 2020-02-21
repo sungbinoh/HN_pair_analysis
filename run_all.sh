db1a9635cd0a2edda7bcfb7e96b2bc67848a1bd5
Sungbin Oh <sungbin.oh@cern.ch>
2020-02-21 14:03:45 +0900

    added miniAOD list

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
-#root -l -b -q "src/QuickPlot.C(2018)"
-#root -l -b -q "src/QuickPlot.C(2017)"
-#root -l -b -q "src/QuickPlot.C(2016)"
+root -l -b -q "src/QuickPlot.C(2018)"
+root -l -b -q "src/QuickPlot.C(2017)"
+root -l -b -q "src/QuickPlot.C(2016)"
 root -l -b -q "src/QuickPlotFullRun2.C(2019)"
 
 #scp -r ./plots/2018/* suoh@147.47.242.44:/home/suoh/Dropbox/HN_pair/2018/
 #scp -r ./plots/2017/* suoh@147.47.242.44:/home/suoh/Dropbox/HN_pair/2017/
 #scp -r ./plots/2016/* suoh@147.47.242.44:/home/suoh/Dropbox/HN_pair/2016/
-scp -r ./plots/2019/* suoh@147.47.242.44:/home/suoh/Dropbox/HN_pair/2019/
+#scp -r ./plots/2019/* suoh@147.47.242.44:/home/suoh/Dropbox/HN_pair/2019/
